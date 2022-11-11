aa1df52b1afe9667ff94653ce087095559b789fb
Berkay ULUĞ <berkay_20o0@hotmail.com>
2022-11-11 12:09:00 +0300

    added

---
+++ app.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const port = 3000
+const hostname = '127.0.0.1'
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1/ihale');
+
+app.use(express.static('public'))
+app.engine('handlebars',exphbs.engine())
+app.set('view engine','handlebars')
