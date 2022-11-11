5d1eb5beedc2ffeb6b012468fda133eb5bbb9151
Berkay ULUĞ <berkay_20o0@hotmail.com>
2022-11-11 15:19:18 +0300

    js bootstrap added

---
+++ models/Post.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const PostSchema = new mongoose.Schema({
+    title:{type:String, required:true},  
+    content:{type:String, required:true},
+    date:{type:Date, default: Date.now}
+})
+
+
+
+const Postlar = mongoose.model('Posts,',PostSchema)
+
+module.exports = mongoose.model('Post',PostSchema)
