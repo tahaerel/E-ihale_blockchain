a2b2fb6d75be698de236c2301c0ef376e489e062
Taha Erel <tahaerelll@gmail.com>
2022-11-12 22:04:39 +0300

    berkay selam görselleri veritabanına yükletip konumu dinamik çekiyoruz artık

--- Routes/posts.js
+++ Routes/posts.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 const Post = require('../models/Post')
+const path = require('path')
+
 
     router.get('/yeni', (req, res) => { res.render('create-item')})     
 
@@ -15,8 +17,16 @@ const Post = require('../models/Post')
 
     router.post('/test',(req,res) =>
     {   
-         Post.create(req.body)
-         console.log(req.files.post_image)
+
+        let post_image = req.files.post_image
+        post_image.mv(path.resolve(__dirname,'../public/assets/images/postimages',post_image.name))
+         Post.create(
+            {
+            ...req.body,
+            post_image:`/assets/images/postimages/${post_image.name}`
+            })
+
+         console.log(req.files.post_image.name)
          res.redirect('/')
     })
 
