f9be0df27d0bbe52a9a5ea3fae291ed830b4a2ab
Taha Erel <tahaerelll@gmail.com>
2022-11-13 10:38:27 +0300

    bugs fixed

--- models/Post.js
+++ models/Post.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
 
 const PostSchema = new mongoose.Schema({
-    title:{type:String, required:true},  
-    content:{type:String, required:true},
+    title:{type:String},  
+    content:{type:String},
     date:{type:Date, default: Date.now},
-    post_image: {type:String,required:true}
+    post_image: {type:String}
 })
 
 
